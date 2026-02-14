86dfa87949f36ae5b449ec2ed1ef441ca1a17e97
JooviDev <joaovictor-contato@hotmail.com>
2026-02-13 21:24:18 -0300

    postgresSQL-Add

--- src/services/convidadosServices.js
+++ src/services/convidadosServices.js
@@ -1,32 +1,36 @@
-import { db } from "../database/dados.js";
+import pool from "../database/connection.js"
 
 export async function CREATE(body = {}) {
   const { nome, checkbox } = body;
 
-  await db.run("INSERT INTO convidados (nome, checkbox) VALUES (?, ?)", [
+  await pool.query("INSERT INTO convidados (nome, checkbox) VALUES ($1, $2)", [
     nome,
     checkbox,
   ]);
 }
 
 export async function listAll() {
-  return db.all("SELECT * FROM convidados");
+  const result = await pool.query("SELECT id, nome, checkbox FROM convidados ORDER BY id DESC");
+
+  return result.rows;
 }
 
 export async function deletar(params = {}) {
   const { id } = params;
-  return await db.run("DELETE FROM convidados WHERE id = ?", [id]);
+  return await pool.query("DELETE FROM convidados WHERE id = $1", [id]);
 }
 
 export async function search(query = {}) {
   const { nome } = query;
 
   if(!nome) {
-    return await db.all("SELECT * FROM convidados")
+    return await pool.query("SELECT id, nome, checkbox FROM convidados ORDER BY id DESC")
   }
 
-  return await db.all(
-    "SELECT * FROM convidados WHERE nome LIKE ?",
+  const result =  await db.all(
+    "SELECT id, nome FROM convidados WHERE nome ILIKE $1",
     [`%${nome}%`],
   );
+
+  return result.rows;
 }
